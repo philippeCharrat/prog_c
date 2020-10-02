26a621e7bc84067b6f4d93f2de2f6ffa56808bc0
philippeCharrat <charrat.p@gmail.com>
2020-10-02 12:57:35 +0200

    ajout de chaine.c

---
+++ TP2/chaine.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/*
+    Nom : fibonacci.c
+    Auteur : CHARRAT Philippe & BRUYERE Axel 
+    Objectif : Ce code va simuler la suite de Fibonacci (https://fr.wikipedia.org/wiki/Suite_de_Fibonacci) à n itérations, avec n saisie par l'utilisateur. Le code fonctionne sous la forme d'une boucle for qui va calculer et afficher le ième termes (exclu, les termes 1,2,3).
+    Remarque : Si l'utilisateur entre une valeur inférieur à 3, cela est une valeur interdite donc on affiche les 3 premières itérations puis on finit le programme.
+*/
+
+int main(int argc, char ** argv) {
+
+    	//-- Initialisations des variables --
+        char message[50] ;
+	char message2[50] ;
+	printf("Veuillez saisir votre message : ");	
+	for (int i =0; i <= 50; i++) {	
+		if(message[i] == '\0')  {
+			break;
+		} 
+	}
+	i = i -2; 
+	switch(cas) {
+	case '0':
+		printf("il y a %d valeurs dans la chaine 1",i);
+		break;
+	case '1' :
+		char message3[50];
+		for(int j=0; j <= i;j++) {
+			 message3[j] = message[j]
+		}
+		printf("le message est  
+
+	}
+
+	return(0);
+
+}
+
