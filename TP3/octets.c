c56fe4912778a4ec3a47fee7517264b0bb36ca30
philippeCharrat <charrat.p@gmail.com>
2020-10-09 10:17:53 +0100

    Add octets.c pas fonctionnel

---
+++ TP3/octets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ *
+ */
+
+int main(void) {
+
+	//--- Initialisation des differentes variables ---
+	int entier; 
+	int long entier_long;
+	float point;
+	double point_double;
+	double long point_double_long;
+
+	// --- Initialisations des pointeurs ---
+	int *ptr_entier = &entier; 
+	int long *ptr_entier_long = &entier_long;
+	float *ptr_point = &point; 
+	double *ptr_point_double = &point_double;
+	double long *ptr_point_double_long = &point_double_long;
+
+
+}
