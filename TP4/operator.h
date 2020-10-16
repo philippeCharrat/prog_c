1918e70a7dc5771ecda698050d2742c8ab8b3443
AxelBruyere <ax.bru99@gmail.com>
2020-10-16 16:53:38 +0200

    changement du nom du fichier operator.c

---
+++ TP4/operator.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/*
+    Nom : operator.h
+    Auteur : CHARRAT Philippe & BRUYERE Axel
+    Objectif : Ce code à pour fonction de pré-définir les fonctions utiles dans operateur.c 
+    Remarque : Les fonctions ont été prédéfinies dans le programme operateur.h
+*/
+void somme(int, int);
+void soustraction(int, int);
+void multiplication(int, int);
+void modulo(int, int);
+void division(int, int);
+void et(int,int);
+void ou(int,int);
