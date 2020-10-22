8165e5a5bfb8ecfabc5caa47ff171f8ee9617e6f
Axel <ax.bru99@gmail.com>
2020-10-22 05:58:00 -0400

    modification de fichier.c (exo1)

--- TP4/fichier.c
+++ TP4/fichier.c
@@ -7,6 +7,7 @@
 #include"fichier.h"
 #include<string.h>
 
+
 //Nom : fichier.c
 //Auteurs : Philippe CHARRAT et Axel BRUYERE 
 //Objectif : Ecrire un programme qui est capable d'ouvrir un fichier .txt et d'y écrire un message entré par l'utilisateur 
@@ -31,7 +32,7 @@ int lire_fichier(char*nom_de_fichier){
     //Fonction d'écriture dans le fichier .txt
 int ecrire_dans_fichier(char *nom_de_fichier, char *message){  
     int fichier,size;
-    fichier = open(nom_de_fichier,O_WRONLY);
+    fichier = open(nom_de_fichier,O_WRONLY|O_CREAT|O_APPEND,S_IRUSR|S_IWUSR);
     size = write(fichier,message,strlen(message));
     close(fichier);    
     return(0);
@@ -39,10 +40,11 @@ int ecrire_dans_fichier(char *nom_de_fichier, char *message){
 
     //On demande à l'utilisateur son message, on l'écrit dans le fichier grâce à la fonction d'écriture, puis on lit le dichier avec la fonction de lecture/affichage afin de vérifier que l'écriture se passe correctement
 int main(){
-    char message[1000];
+    char message[1000] = {};
     printf("Veuillez saisir votre message : ");
     scanf("%s",message);
     char *nom_de_fichier = "fichier.txt";
+    int return_code = remove(nom_de_fichier);
     int fichier = ecrire_dans_fichier(nom_de_fichier,message);
     int k = lire_fichier(nom_de_fichier);
     return(0);
