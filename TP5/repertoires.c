5aaf481b592780ed316b476f75fe352fdb4e94f3
philippeCharrat <charrat.p@gmail.com>
2020-10-23 10:57:43 +0100

    Fin de la partie recursive de la fonction

--- TP5/repertoires.c
+++ TP5/repertoires.c
@@ -15,22 +15,13 @@
 // 	- ptr vers le lien du dossier a afficher
 // Outputs : 
 // 	- none
-void lire_dossier(char * buffer,int indent) {
+void lire_dossier(char * buffer) {
     // Initialisation des variables utiles 
     DIR *dir;
-    struct dirent *structure_fichier;
-    char espace[10];
+    struct dirent *structure_fichier; 
 
     // Ouverture du dosssier 
     dir = opendir(buffer);
-
-    for(int i=0;i <= indent;i++) {
-	    if (i == indent) { 
-		    espace[i] = '\0';
-	    } else {
-		    espace[i] = '-';
-	    }
-    }
 
     // Si le fichier existe
     if(dir!=NULL) {
@@ -40,7 +31,6 @@ void lire_dossier(char * buffer,int indent) {
 	// Tant que le dossier est non-nul, on affiche les fichiers 
         while(structure_fichier!=NULL) {
             // Affichage du nom de dossier
-	    printf("%s",&(espace[0]));
             printf(structure_fichier->d_name);
     	    printf("\n");
     	    // On teste de recuperer la structure du fichier suivante 
@@ -49,49 +39,69 @@ void lire_dossier(char * buffer,int indent) {
     }
 
     // On ferme le fichier 
-    close(dir);
+    closedir(dir);
 }
 
-void listdir(char *buffer) {
+void listdir(char *buffer,int indent) {
     // Initialisation des variables utiles 
     DIR *dir;
     struct dirent *structure_fichier;
-    int i = 0; 
+    char espace[15];
+    char lien_parent[100];
+
     // Ouverture du dosssier 
     dir = opendir(buffer);
     
     // Si le fichier existe
+    for(int i=0;i <= indent;i++) {
+	    if (i == indent) { 
+		    espace[i] = '\0';
+	    } else {
+		    espace[i] = '-';
+	    }
+    }
+
     if(dir!=NULL) {
 
         structure_fichier=readdir(dir);
-        while (structure_fichier != NULL || !(structure_fichier -> d_name == "..")) {
-		
+	while (structure_fichier != NULL )  {
             // Test du type du fichier 
-            if (structure_fichier->d_type == DT_DIR) {
-		i += 1;
-                lire_dossier(structure_fichier->d_name,i);
+	    memset(lien_parent,0,sizeof(lien_parent));
+	    strcpy(lien_parent,buffer);   
+	    strcat(lien_parent,"/");
+            if (structure_fichier->d_type == 4 && strcmp(structure_fichier->d_name,"..")  != 0 && strcmp(structure_fichier->d_name,".")!=0) {
+    		strcat(lien_parent,structure_fichier->d_name);
+		printf("\033[0;32m");
+		printf("%s %s\n",&espace,&lien_parent);
+		printf("\033[0m");
+
+                listdir(lien_parent,indent+3);
+		printf("\n");
             } 
             else { 
+		printf("%s",&espace);
 		printf(structure_fichier->d_name);
 		printf("\n");
 	    }
             structure_fichier=readdir(dir);
         }
-    }
-    close(dir);
+    } else {printf("fin du buzz");}
+    closedir(dir);
 }
 
 
 int main() {
     // Initialisations des variables utiles 	
     char buffer[50];
-    int indent = 0;
+    int indent = 1;
+
     // Saisie du lien du repertoire
     printf("Saisir un lien  : ");
     scanf("%s",&(buffer[0]));
+    printf("Les dossiers et sous dossiers de %s sont : \n",&buffer);
 
     // Appel de la fonction lire_dosier 
-    listdir(buffer);
+    listdir(buffer,indent);
 
     return(0);
 }   
