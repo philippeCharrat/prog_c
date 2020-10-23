88beceacad432b93b1834790f524fc5409bdfd20
philippeCharrat <charrat.p@gmail.com>
2020-10-23 12:46:11 +0200

    ajout de l'exo 3 peut être

--- TP5/repertoires.c
+++ TP5/repertoires.c
@@ -8,6 +8,8 @@
  * 	But : Saisir le nom d'un dossier et le parcourir
  * 	Remarque : 
  *      - ce code est inspiré de : https://stackoverflow.com/questions/3554120/open-directory-using-c
+ *      - La partie sous dossier a été inspirée de : https://stackoverflow.com/questions/8436841/how-to-recursively-list-directories-in-c-on-linux
+ *      - Focntion opendir : http://manpagesfr.free.fr/man/man3/opendir.3.html
  */
 
 // Fonction affichage des dossiers :
@@ -42,7 +44,13 @@ void lire_dossier(char * buffer) {
     closedir(dir);
 }
 
-void listdir(char *buffer,int indent) {
+// Fonction affichage des dossiers et sous dossiers  :
+// Inputs : 
+//  - ptr vers le lien du dossier a afficher
+//  - int pour l'affichage 
+// Outputs : 
+//  - none
+void lire_dossier_it(char *buffer,int indent) {
     // Initialisation des variables utiles 
     DIR *dir;
     struct dirent *structure_fichier;
@@ -52,7 +60,7 @@ void listdir(char *buffer,int indent) {
     // Ouverture du dosssier 
     dir = opendir(buffer);
     
-    // Si le fichier existe
+    // Création d'un niveau d'intenation esthétique
     for(int i=0;i <= indent;i++) {
 	    if (i == indent) { 
 		    espace[i] = '\0';
@@ -61,31 +69,43 @@ void listdir(char *buffer,int indent) {
 	    }
     }
 
+    // Si l'on peut ouvrir le dossiers, on le parcours
     if(dir!=NULL) {
 
         structure_fichier=readdir(dir);
-	while (structure_fichier != NULL )  {
-            // Test du type du fichier 
-	    memset(lien_parent,0,sizeof(lien_parent));
-	    strcpy(lien_parent,buffer);   
-	    strcat(lien_parent,"/");
-            if (structure_fichier->d_type == 4 && strcmp(structure_fichier->d_name,"..")  != 0 && strcmp(structure_fichier->d_name,".")!=0) {
-    		strcat(lien_parent,structure_fichier->d_name);
-		printf("\033[0;32m");
-		printf("%s %s\n",&espace,&lien_parent);
-		printf("\033[0m");
+    	while (structure_fichier != NULL )  {
+            // Génération du nouveau lien parents 
+    	    memset(lien_parent,0,sizeof(lien_parent));
+    	    strcpy(lien_parent,buffer);   
+    	    strcat(lien_parent,"/");
 
-                listdir(lien_parent,indent+3);
-		printf("\n");
-            } 
+            // Si le fichier est un directory et différent de . et .. alors on fait une récursion
+            if (structure_fichier->d_type == 4 && strcmp(structure_fichier->d_name,"..")  != 0 && strcmp(structure_fichier->d_name,".")!=0) {
+                // Génération du lien complet
+                strcat(lien_parent,structure_fichier->d_name);
+
+                // Affichage du lien complet (en vert)
+        		printf("\033[0;32m");
+        		printf("%s %s\n",&espace,&lien_parent);
+        		printf("\033[0m");
+
+                // Appel de la récursion
+                lire_dossier(lien_parent,indent+3);
+                printf("\n");
+            }
+            // Sinon on affiche le fichier avec une indentation  
             else { 
-		printf("%s",&espace);
-		printf(structure_fichier->d_name);
-		printf("\n");
-	    }
+        		printf("%s",&espace);
+        		printf(structure_fichier->d_name);
+        		printf("\n");
+            }
+
+            // Prochain fichier
             structure_fichier=readdir(dir);
-        }
-    } else {printf("fin du buzz");}
+        }   
+    } 
+
+    // Fermeture du fichier 
     closedir(dir);
 }
 
@@ -101,7 +121,7 @@ int main() {
     printf("Les dossiers et sous dossiers de %s sont : \n",&buffer);
 
     // Appel de la fonction lire_dosier 
-    listdir(buffer,indent);
+    lire_dossier_rec(buffer,indent);
 
     return(0);
 }   
